f42dbf28d58047e0e19ec7ea1aa3184d2e5f0b4a
domi802 <64065747+domi802@users.noreply.github.com>
2020-04-23 20:50:36 +0200

    card update

--- sklep/carts/urls.py
+++ sklep/carts/urls.py
@@ -5,5 +5,7 @@ from django.urls import re_path
 
 
 urlpatterns = [
+    re_path(r'^cart/(?P<slug>[\w-]+)/$', views.update_cart, name='update_cart'),
     re_path(r'^cart/$', views.view, name='cart'),
+
     ]
--- sklep/carts/views.py
+++ sklep/carts/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from products.models import product
 from .models import Cart
 # Create your views here.
 
 def view(request):
+    
     cart = Cart.objects.all()[0]
     context = {"cart": cart}
     template = "cart/koszyk.html"
     return render(request, template, context)
+
+
+def update_cart(request, slug):
+    cart = Cart.objects.all()[0]
+    basket = product.objects.get(slug=slug)
+    if not basket in cart.products.all():
+        cart.products.add(basket)
+    else:
+        cart.products.remove(basket) 
+    new_total = 0.00 
+    for item in cart.products.all():
+        new_total += float(item.price)
+
+    cart.total = new_total
+    cart.save()    
+    return HttpResponseRedirect(reverse("cart")) 
